d02eb13c9847dec309fe1670e54ddfc88485207e
sivakumar <kctsiva@gmail.com>
2023-09-06 20:00:27 +0530

    Create console-log-tenant-config.js

---
+++ console-log-tenant-config.js
@@ -0,0 +1,5 @@
+console.log('Logs from "console-log-tenant-config.js"')
+
+console.log("window.tenantAppConfig =>", window.tenantAppConfig)
+
+setTimeout(() => console.log("window.tenantAppConfig after 2 secs =>", window.tenantAppConfig), 2000)
